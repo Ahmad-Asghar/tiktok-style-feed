9ca9b6c0bc0a749b17a0cf5c123733da1ddbc262
Ahmad Asghar <ahmadrajpoot0305@gmail.com>
2025-06-04 18:30:17 +0500

    final

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,13 +3,14 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id "com.google.gms.google-services"
+    id("com.google.gms.google-services") version "4.4.2" apply false
+
 }
 
 android {
     namespace = "com.example.voicer_writer"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -25,7 +26,7 @@ android {
         applicationId = "com.example.voicer_writer"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -44,6 +45,6 @@ flutter {
     source = "../.."
 }
 dependencies {
-    implementation platform("com.google.firebase:firebase-bom:33.3.0")
-    implementation "com.google.firebase:firebase-analytics"
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
